0627666e748fdfd8be61bfca1cacaf77268883b3
sansita0704 <sansita7406@gmail.com>
2025-11-19 14:18:50 +0530

    link team page

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,45 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Testimonial = () => {
-  return (
-    <div id="testimonial">
-      <h2>Teams</h2>
+    return (
+        <div id="testimonial">
+            <h2>Teams</h2>
 
-      <section>
-        <TestimonialCard
-          name={"Technical Team"}
-          feedback={"Innovating solutions, enhancing efficiency."}
-        />
+            <section>
+                <Link to={"/team/#tech-team"}>
+                    <TestimonialCard
+                        name={"Technical Team"}
+                        feedback={"Innovating solutions, enhancing efficiency."}
+                    />
+                </Link>
 
-        <TestimonialCard
-          name={"Event-Management Team"}
-          feedback={
-            "Seamlessly organizing memorable events."
-          }
-        />
+                <Link to={"/team/#event-team"}>
+                    <TestimonialCard
+                        name={"Event-Management Team"}
+                        feedback={"Seamlessly organizing memorable events."}
+                    />
+                </Link>
 
-        <TestimonialCard
-          name={"Content-Writing Team"}
-          feedback={"Crafting impactful and engaging stories."}
-        />
-        <TestimonialCard
-          name={"Digital-Design Team"}
-          feedback={"Creating visually stunning digital experiences."}
-        />
-      </section>
-    </div>
-  );
+                <Link to={"/team/#content-team"}>
+                    <TestimonialCard
+                        name={"Content-Writing Team"}
+                        feedback={"Crafting impactful and engaging stories."}
+                    />
+                </Link>
+
+                <Link to={"/team/#graphic-team"}>
+                    <TestimonialCard
+                        name={"Digital-Design Team"}
+                        feedback={
+                            "Creating visually stunning digital experiences."
+                        }
+                    />
+                </Link>
+            </section>
+        </div>
+    );
 };
 
 const TestimonialCard = ({ name, feedback }) => (
-  <article>
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-      alt="User"
-    />
-    <h4>{name}</h4>
-    <p>{feedback}</p>
-  </article>
+    <article>
+        <img
+            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+            alt="User"
+        />
+        <h4>{name}</h4>
+        <p>{feedback}</p>
+    </article>
 );
 
 export default Testimonial;
