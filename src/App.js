e7a09e08c4c4c9690cd100300c53713f77505d73
Sansita Jain <sansita7406@gmail.com>
2026-08-23 22:03:16 +0530

    Resolve merge conflict in App component

--- src/App.js
+++ src/App.js
@@ -9,12 +9,11 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
-import GlobalStyle from './global.scss';
+import "./global.scss";
 import Team from "./components/Team";
 import { createBrowserRouter } from "react-router-dom";
 
 export function App() {
-  <GlobalStyle /> 
   const [menuOpen, setMenuOpen] = useState(false);
   const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
   useEffect(() => {
